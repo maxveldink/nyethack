d985706d3a8fead97c012cd2f0fd6e8e340e22af
Max VelDink <maxveldink@gmail.com>
2018-12-31 16:17:43 -0600

    Up to chapter 14

---
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.nyethack
+
+fun main(args: Array<String>) {
+    val player = Player("Madrigal")
+    player.castFireball()
+
+    // com.bignerdranch.nyethack.Player Status
+    printPlayerStatus(player)
+}
+
+private fun printPlayerStatus(player: Player) {
+    println(
+        "(Aura: ${player.auraColor()}) " +
+                "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
+    )
+    println("${player.name} ${player.formatHealthStatus()}")
+}
---
+++ src/com/bignerdranch/nyethack/Tavern.kt
@@ -0,0 +1,90 @@
+package com.bignerdranch.nyethack
+
+import java.io.File
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
+val menuList = File("data/tavern-menu-items.txt")
+                    .readText()
+                    .split("\n")
+
+val patronGold = mutableMapOf<String, Double>()
+
+fun main(args: Array<String>) {
+    if (patronList.contains("Eli")) {
+        println("The tavern master says: Eli's in the back playing cards.")
+    } else {
+        println("The tavern master says: Eli isn't here.")
+    }
+
+    if (patronList.containsAll(listOf("Sophie", "Mordoc"))) {
+        println("The tavern master says: Yea, they're seated by the stew kettle.")
+    } else {
+        println("The tavern master says: Nay, they departed hours ago.")
+    }
+
+    (0..9).forEach {
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first $last"
+        uniquePatrons += name
+    }
+    uniquePatrons.forEach {
+        patronGold[it] = 6.0
+    }
+
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(
+            uniquePatrons.shuffled().first(),
+            menuList.shuffled().first()
+        )
+        orderCount++
+    }
+
+    displayPatronBalances()
+}
+
+private fun displayPatronBalances() {
+    patronGold.forEach { patron, balance ->
+        println("$patron, balance: ${"%.2f".format(balance)}")
+    }
+}
+
+fun performPurchase(price: Double, patronName: String) {
+    val totalPurse = patronGold.getValue(patronName)
+    patronGold[patronName] = totalPurse - price
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiouAEIOU]")) {
+        when (it.value) {
+            "a", "A" -> "4"
+            "e", "E" -> "3"
+            "i", "I" -> "1"
+            "o", "O" -> "0"
+            "u", "U" -> "|_|"
+            else -> it.value
+        }
+    }
+
+private fun placeOrder(patronName: String, menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("$patronName speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "$patronName buys a $name ($type) for $price."
+    println(message)
+    performPurchase(price.toDouble(), patronName)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "$patronName exclaims ${toDragonSpeak("Ah, DELICIOUS $name")}"
+    } else {
+        "$patronName says: Thanks for the $name."
+    }
+    println(phrase)
+}
